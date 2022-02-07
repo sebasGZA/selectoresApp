6fb6b92fcae692aafa761cfc06fca359c118d90e
sebasGZA <sebassga34@gmail.com>
2022-02-07 06:11:02 -0500

    Selector de regiones

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PaisesService } from '../../services/paises.service';
 
 @Component({
   selector: 'app-selector-page',
@@ -13,9 +14,13 @@ export class SelectorPageComponent implements OnInit {
     region: ['', [Validators.required]]
   })
 
-  constructor(private formBuilderSvc: FormBuilder) { }
+  //Llenar selectores
+  regiones: string[] = []
+
+  constructor(private formBuilderSvc: FormBuilder, private paisesSvc: PaisesService) { }
 
   ngOnInit(): void {
+    this.regiones = this.paisesSvc.regiones
   }
 
   guardar() {
